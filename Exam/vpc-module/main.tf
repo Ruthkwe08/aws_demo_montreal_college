373a4a56a4a1fe1836ff4a0c2b6f339d66754111
Ruthkwe08 <146134486+Ruthkwe08@users.noreply.github.com>
2024-02-08 19:55:06 -0500

    Create main.tf

---
+++ Exam/vpc-module/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region = "us-east-1"  # Change to your preferred region
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "MyVPC"
+  }
+}
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"  # Change to your preferred AZ
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "SubnetA"
+  }
+}
