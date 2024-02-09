be6ad130ad2dc0f6d001b190204c5253add417ec
Ruthkwe08 <146134486+Ruthkwe08@users.noreply.github.com>
2024-02-08 20:00:24 -0500

    Update main.tf

--- Exam/vpc-module/main.tf
+++ Exam/vpc-module/main.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "subnet_a" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"  # Change to your preferred AZ
+  availability_zone       = "us-east-1a"  
   map_public_ip_on_launch = true
   tags = {
     Name = "SubnetA"
